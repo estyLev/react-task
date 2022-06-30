f0060adfe168e959e74b345ea20c45d247747736
estyLev <st0533184039@gmail.com>
2022-06-30 18:13:48 +0300

    commited

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Esty lev!!</p>
+        <p>Esty lev!!!</p>
         <GhPages></GhPages>
         <p>
           Edit <code>src/App.js</code> and save to reload.
